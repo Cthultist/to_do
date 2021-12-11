4cc52a21edc3b07d02aade6717cdcf39577ae314
Cthultist <94089936+Cthultist@users.noreply.github.com>
2021-12-10 18:17:45 -0800

    Task structs, and enums established. can print value of task struct.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-// use crossterm::{event, cursor, style, terminal};
-// use std::io::{stdout, Write, Read};
-// use std::collections::HashMap;
+use std::io;
 
 fn main() {
-    println!("You're doing it!")
+    let task1 = Task {
+        description: String::from("Make Dinner"),
+        state: TaskState::Incomplete,
+    };
+    println!("You're doing it!, Task is {:?}", task1)
 }
 
+#[derive(Debug)]
 struct Task {
     description: String,
     state: TaskState,
 }
 
+impl Task {
+    fn new(self) -> Self {
+        Task {
+            description: String::from("New Task"),
+            state: TaskState::Incomplete,
+        }
+    }
+}
+
+#[derive(Debug)]
 enum TaskState {
     Incomplete,
     InProgress,
     Complete,
+}
+
+fn new_task(task_name: &str) -> io::Result<()> {
+    let mut task_name = String::new();
+    let task_name = io::stdin().read_line(&mut task_name).unwrap();
+    Ok(())
 }
