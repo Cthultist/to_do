3369abf08b1b359a6305620f7ee6cb29dcac898a
Cthultist <94089936+Cthultist@users.noreply.github.com>
2021-12-14 19:52:03 -0800

    todo.. save function, and complete function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::io;
+use std::io::Read;
 
 fn main() {
-    let task1 = Task {
-        description: String::from("Make Dinner"),
-        state: TaskState::Incomplete,
-    };
-    println!("You're doing it!, {:?}", task1)
+    let action = std::env::args().nth(1).expect("Please specify an action.");
+    let task_name = std::env::args().nth(2).expect("Please specify an item");
+    
+    if action == "add" {
+        let mut new_task = Task::new().expect("Initialization of db failed");
+        match task.save() {
+            Ok(_) => println!("Task Saved!"),
+            Err(why) => println!("An error occurred: {}", why)
+        } 
+    } else if action == "complete" {
+        match task.complete(&task_name) {
+            None => println!("'{}' is not present in the list", task_name),
+            Some(_) => match task.save() {
+                Ok(_) => println!("To-do updated!"),
+                Err(why) => println!("An error Occurred: {}", why),
+            }
+        }
+    }
+    // println!("{:?}, {:?}", task_name, task_state);
+    // let task1 = Task {
+    //     description: String::from("Make Dinner"),
+    //     state: TaskState::Incomplete,
+    // };
+    // println!("You're doing it!, {:?}", task1)
 }
 
 //todo add float point numbers to tasks for task nesting and possibly for ease of editing or removing tasks.
@@ -16,11 +35,22 @@ struct Task {
 }
 
 impl Task {
-    fn new(self) -> Self {
-        Task {
-            description: String::from("New Task"),
-            state: TaskState::Incomplete,
-        }
+    fn new() -> Result<Task, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        std::fs::read_to_string("db.txt")
+            .expect("file not found!")
+            .lines()
+            .map(|x| x.parse())
+            .collect()
+    }
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = self.new_task;
+        
+        std::fs::write("db.txt", self)
     }
 }
 
@@ -31,8 +61,8 @@ enum TaskState {
     Complete,
 }
 
-fn new_task(task_name: &str) -> io::Result<()> {
-    let mut task_name = String::new();
-    let task_name = io::stdin().read_line(&mut task_name).unwrap();
-    Ok(())
-}
+// fn new_task(task_name: &str) -> io::Result<()> {
+//     let mut task_name = String::new();
+//     let task = io::stdin().read_line(&mut task_name).unwrap();
+//     Ok(())
+// }
